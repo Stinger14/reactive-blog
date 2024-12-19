d2c55174a4ee2fa990a6ee3d197d398d20f41f1a
Maxly Garcia <maxgarcia6@gmail.com>
2024-12-19 15:19:54 -0400

    Feat: create comments related to a specific post

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
 
 export default () => {
     // define a state variable posts and initialize it as an empty object
@@ -27,6 +28,7 @@ export default () => {
             >
             <div className='card-body'>
                 <h3>{post.title}</h3>
+                <CommentCreate postId={post.id} />
             </div>
         </div>
     );
