222a20d91ee9dcba3740b59170c897c244c5ee2a
Maxly Garcia <maxgarcia6@gmail.com>
2024-12-27 15:37:12 -0400

    Feat: create moderation service

---
+++ moderation/index.js
@@ -0,0 +1,16 @@
+// Service: watch for events
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    // const { type, data } = req.body;
+});
+
+app.listen(4003, () => {
+    console.log('Listening on 4003');
+});
