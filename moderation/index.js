481f08470ec78c57be4e93c1d3ff68ac7c4899d2
Maxly Garcia <maxgarcia6@gmail.com>
2024-12-28 02:15:39 -0400

    Update: implement event handler for the moderation service. Push new event to the event bus.

--- moderation/index.js
+++ moderation/index.js
@@ -7,8 +7,25 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-    // const { type, data } = req.body;
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status
+            }
+        }).catch(err => {
+            console.log(err.message);
+        });
+    }
+    res.send({});
 });
 
 app.listen(4003, () => {
