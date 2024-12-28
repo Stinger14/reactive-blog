1cdeabb4433aedd9495218300165580fcad2d098
Maxly Garcia <maxgarcia6@gmail.com>
2024-12-28 11:51:46 -0400

    Update: render comments by status

--- query/index.js
+++ query/index.js
@@ -39,6 +39,17 @@ app.post('/events', (req, res) => {
         post.comments.push({ id, content, status });
     }
 
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+        comment.content = content;
+    }
+
     console.log(posts);
     console.log(data);
     console.log('Received Event', req.body.type);
